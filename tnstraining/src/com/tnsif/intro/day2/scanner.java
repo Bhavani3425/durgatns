62b125a155ed4d1ec7592fa38d5d58a9804bd654
Dhurga bavani <Dhurga bavani@LAPTOP-33KV9BK1>
2025-12-29 14:33:01 +0530

    scanner

---
+++ tnstraining/src/com/tnsif/intro/day2/scanner.java
@@ -0,0 +1,30 @@
+package com.tnsif.intro.day2;
+
+import java.util.Scanner;
+
+public class scanner {
+
+	public static void main(String[] args) {
+		Scanner sac=new Scanner(System.in);
+		System.out.println("Enter number:" );
+        int a=sac.nextInt();
+        switch(a) {
+		case 1:
+			System.out.println("Best");
+			break;
+		case 2:
+			System.out.println("Better");
+			break;
+		case 3:
+			System.out.println("Good");
+			break;
+		case 4:
+			System.out.println("Nice");
+			break;
+		default:
+			System.out.println("Worst");
+			break;
+	}
+
+}
+}

5786ad2b26f36e9116dee20be25660666bb3749b
Dhurga bavani <Dhurga bavani@LAPTOP-33KV9BK1>
2025-12-29 14:51:12 +0530

    lop

---
+++ tnstraining/src/com/tnsif/intro/day3/whileloop.java
@@ -0,0 +1,17 @@
+package com.tnsif.intro.day3;
+
+public class whileloop {
+
+	public static void main(String[] args) {
+		int a=2;
+		while(a<11) {
+			System.out.println(a);
+			a=a+2;
+	}
+
+}
+
+
+	}
+
+

310c60defc41be962eeebb92c75d11735bdbb831
Dhurga bavani <Dhurga bavani@LAPTOP-33KV9BK1.lan>
2025-12-29 21:18:12 +0530

    fghj

---
+++ tnstraining/src/construct/def.java
@@ -0,0 +1,19 @@
+package construct;
+
+public class def {
+
+	int sid=101;
+	String sname="bhavani";
+	def(){
+		System.out.println("The student name:"+sname);
+		}
+	def(int a){
+		System.out.println("Marks "+a+" got to "+sname);
+	}
+	def(int a,int b){
+		System.out.println("value1 "+a+" value2 "+b);
+		
+		
+	}
+
+}

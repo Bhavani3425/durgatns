47c5a662ec9924318f32df49609673fd77c8cfce
Dhurga bavani <Dhurga bavani@LAPTOP-33KV9BK1>
2025-12-29 14:40:53 +0530

    drt

---
+++ tnstraining/src/com/tnsif/intro/day2/inbuiltscanprint.java
@@ -0,0 +1,22 @@
+package com.tnsif.intro.day2;
+import java.util.Scanner;
+public class inbuiltscanprint {
+
+	public static void main(String[] args) {
+		
+		Scanner sac=new Scanner(System.in);
+		System.out.println("Enter number:" );
+        int a=sac.nextInt();
+        if(a%2==0) {
+			System.out.println("Even");
+		}
+		else {
+		System.out.println("Odd");
+
+
+	}
+
+}
+
+
+}

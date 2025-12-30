0d0b985c91f70edce33cb37f00c56ffe73b27ac0
Dhurga bavani <Dhurga bavani@LAPTOP-33KV9BK1>
2025-12-30 13:38:48 +0530

    construt

---
+++ tnstraining/src/com/tnsif/intro/construct/Demo.java
@@ -0,0 +1,14 @@
+package com.tnsif.intro.construct;
+
+public class Demo {
+
+	public static void main(String[] args) {
+			def st=new def();
+			def st1=new def(100);
+			def st2=new def(90,80);
+		}
+
+
+	
+
+}
---
+++ tnstraining/src/com/tnsif/intro/construct/def.java
@@ -0,0 +1,19 @@
+package com.tnsif.intro.construct;
+
+public class def {
+	int sid=101;
+	String sname="Durga";
+	def(){
+		System.out.println("The student name:"+sname);
+		}
+	def(int a){
+		System.out.println("Marks "+a+" got to "+sname);
+	}
+	def(int a,int b){
+		System.out.println("value1 "+a+" value2 "+b);
+		
+		
+	}
+
+}
+

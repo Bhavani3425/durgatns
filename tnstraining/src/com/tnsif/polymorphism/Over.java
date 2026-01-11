f0bf1570aa9e3a8a5a77b86548c1eb124f931207
Dhurga bavani <Dhurga bavani@LAPTOP-33KV9BK1>
2026-01-11 13:05:53 +0530

    Poly

---
+++ tnstraining/src/com/tnsif/polymorphism/Over.java
@@ -0,0 +1,20 @@
+package com.tnsif.polymorphism;
+
+public class Over {
+
+	public static void main(String[] args) {
+		
+
+			
+				Overload obj=new Overload();
+				obj.show();
+				obj.show(20);
+				obj.show(60,9.8765);
+				obj.show(1.765678,55, 85.766f);
+			}
+
+		
+
+	}
+
+

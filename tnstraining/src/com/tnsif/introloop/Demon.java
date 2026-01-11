d7ec7a7c1200f5642e694c48ce5759e5873b63ad
Dhurga bavani <Dhurga bavani@LAPTOP-33KV9BK1>
2026-01-11 13:24:31 +0530

    intro

---
+++ tnstraining/src/com/tnsif/introloop/Demon.java
@@ -0,0 +1,18 @@
+package com.tnsif.introloop;
+
+public class Demon {
+
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println(i);
+			i++;
+			}
+		while(i<=10);
+	}
+
+
+
+	}
+
+
---
+++ tnstraining/src/com/tnsif/introloop/Studentmain.java
@@ -0,0 +1,22 @@
+package com.tnsif.introloop;
+
+public class Studentmain {
+
+	public static void main(String[] args) {
+		
+			
+	        Student s = new Student();
+
+	        s.getsid(345);
+	        s.getsname("Durgabhavani");
+	        s.getsmarks(99);
+	        System.out.println("ID: " + s.setsid());
+	        System.out.println("Name: " + s.setsname());
+            System.out.println("Marks:"+s.setsmarks());   
+	        		    }
+	
+
+
+	}
+
+

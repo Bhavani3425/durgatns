f1aae40a16a5921c5415be6e2dbe510f44b37c21
Dhurga bavani <Dhurga bavani@LAPTOP-33KV9BK1>
2026-01-11 13:13:18 +0530

    Wrapper

---
+++ tnstraining/src/com/tnsif/wrapper/Autoboxing.java
@@ -0,0 +1,19 @@
+package com.tnsif.wrapper;
+
+public class Autoboxing {
+
+	public static void main(String[] args) {
+		
+			int a=30;
+			Integer i=new Integer(a);
+			System.out.println(i);
+			float b=23.89f;
+			Float f=b;
+			System.out.println(f);
+		}
+
+	
+
+	}
+
+
---
+++ tnstraining/src/com/tnsif/wrapper/Autounboxing.java
@@ -0,0 +1,18 @@
+package com.tnsif.wrapper;
+
+public class Autounboxing {
+
+	
+	
+
+			public static void main(String[] args) {
+				Double b=45.2345;
+				double g=b;
+				System.out.println(g);
+			}
+
+		
+
+	}
+
+
